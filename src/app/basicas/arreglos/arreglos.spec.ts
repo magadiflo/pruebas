001cf178ea8c3adbbb36e3efcca393b70122b3e4
Martín <magadiflo@gmail.com>
2022-03-24 13:08:02 -0500

    Saltar pruebas

--- src/app/basicas/arreglos/arreglos.spec.ts
+++ src/app/basicas/arreglos/arreglos.spec.ts
@@ -1,6 +1,7 @@
 import { obtenerRobots } from './arreglos';
 
-describe('Prueba de arreglos', () => {
+//con x adelante omitimos las pruebas
+xdescribe('Prueba de arreglos', () => {
     it('Debe de retornar al menos 3 robots', () => {
         const res = obtenerRobots();
         expect(res.length).toBeGreaterThanOrEqual(3);
