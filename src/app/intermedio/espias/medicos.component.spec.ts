d2bac654204d86c9d9a49064bfc701b0e62896a1
Martín <magadiflo@gmail.com>
2022-03-25 10:48:43 -0500

    Explicación del contenido de los archivos adjuntos

---
+++ src/app/intermedio/espias/medicos.component.spec.ts
@@ -0,0 +1,20 @@
+import { MedicosComponent } from './medicos.component';
+import { MedicosService } from './medicos.service';
+
+
+describe('MedicosComponent', () => {
+
+    let componente: MedicosComponent;
+
+    beforeEach( () => {
+        
+    });
+
+
+    it('', () => {
+
+   
+    });
+
+
+});
