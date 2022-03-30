9a56ff1299d20acbda7064e2da658ec3e10627f0
Martín <magadiflo@gmail.com>
2022-03-30 13:03:17 -0500

    Probando la existencia de una ruta en particular

---
+++ src/app/avanzado/rutas/app.routes.spec.ts
@@ -0,0 +1,9 @@
+import { rutas } from './app.routes';
+
+import { MedicoComponent } from '../../intermedio2/medico/medico.component';
+
+describe('Rutas principales', () => {
+    it('Debe de existir la ruta /medico/:id', () => {
+        expect(rutas).toContain({ path: 'medico/:id', component: MedicoComponent });
+    });
+});
