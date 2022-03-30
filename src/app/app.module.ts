83aadabdcc7603d6620dc332b59761fc750d28e6
Martín <magadiflo@gmail.com>
2022-03-30 11:08:21 -0500

    Archivo SPEC generado automáticamente usando AngularCLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MedicoComponent } from './intermedio2/medico/medico.component';
+import { HospitalComponent } from './intermedio2/hospital/hospital.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MedicoComponent
+    MedicoComponent,
+    HospitalComponent
   ],
   imports: [
     BrowserModule
