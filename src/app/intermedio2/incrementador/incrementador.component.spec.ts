21926d00ec5800f7535a8c20749fc11c355f6315
Martín <magadiflo@gmail.com>
2022-03-30 11:56:05 -0500

    Revisar el valor de un input físicamente

--- src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -26,10 +26,22 @@ describe('Incremendator Component', () => {
         const leyenda = 'Progreso de carga...';
         component.leyenda = leyenda;
         fixture.detectChanges(); //Disparar la detección de cambios
-        
+
         const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
 
         expect(elem.innerHTML).toContain(leyenda);
+
+    });
+
+    it('Debe demostrar en el input el valor del progreso', () => {
+        component.cambiarValor(5);
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            const input = fixture.debugElement.query(By.css('input'));
+            const elem = input.nativeElement;
+    
+            expect(elem.value).toBe(55);
+        });
 
     });
 
