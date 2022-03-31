78b9ebadb5ecf67feb02f4b0b9b711ef7f9adab6
Martín <magadiflo@gmail.com>
2022-03-31 09:13:43 -0500

    Confirmar que exista un router-outlet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from "@angular/router/testing";
+import { By } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -25,5 +27,11 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('pruebas');
+  });
+
+  it('Debe de tener un router-outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const debugElement = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(debugElement).not.toBeNull();
   });
 });
